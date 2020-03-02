43d2b9c524a82cc3b663844a1fba6162fa4507db
ygk313 <50168972+ygk313@users.noreply.github.com>
2020-03-02 15:50:30 +0900

    django_pagination

--- django-REST/firstrest/post/views.py
+++ django-REST/firstrest/post/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
 from .models import Post
 from .serializer import PostSerializer
-
+from .pagination import Mypagination
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().order_by('id')
     serializer_class = PostSerializer
+    pagination_class = Mypagination
